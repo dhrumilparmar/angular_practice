a542808789b04793e6a583345329910daa0c9cfd
dhrumilparmar <dhrumilparmar27481@gmail.com>
2026-04-08 15:24:00 +0530

    control flow with if and for

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormPracticeComponent } from '../form-practice/form-practice.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormPracticeComponent,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  show: boolean = false;
+users = [
+  { name: 'Dev', price: 100, date: new Date('2018/1/1') },
+  { name: 'User2', price: 200, date: new Date('2019/2/2') },
+  { name: 'User3', price: 300, date: new Date('2020/3/3') }
+];
+
 
 }
