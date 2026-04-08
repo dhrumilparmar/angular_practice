14d33c29bdacd26625e8c4e3e875408804f2e1ce
dhrumilparmar <dhrumilparmar27481@gmail.com>
2026-04-08 16:35:22 +0530

    variables, @defer, ngcontainer implemented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormPracticeComponent } from '../form-practice/form-practice.component';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -19,6 +18,16 @@ users = [
   { name: 'User2', price: 200, date: new Date('2019/2/2') },
   { name: 'User3', price: 300, date: new Date('2020/3/3') }
 ];
+
+isloading: boolean = true;
+toggle(){
+  this.isloading = !this.isloading;
+}
+
+print(value: string) {
+  console.log('Input value:', value);
+}
+
 
 
 }
